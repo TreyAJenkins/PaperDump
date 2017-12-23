d59c1b274aab76a42c072d48f03a68b032b20d19
Trey Jenkins <treyajenkins@gmail.com>
2017-12-23 18:40:42 -0500

    Cleaned up code

--- PaperDump.py
+++ PaperDump.py
@@ -6,8 +6,6 @@ from fpdf import FPDF
 from Crypto.Cipher import AES
 import binascii, json, zlib, time, base64, qrcode, math, os, shutil, subprocess, magic, hashlib, pdf417gen
 
-lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-#defblocksize = 1850.0
 defblocksize = 1850.0
 pdfblocksize = 800.0
 
